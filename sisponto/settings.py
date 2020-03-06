73e8ae2845d37753cdb093b35b47edd7f7a0efac
williancozzi <williancozzi@gmail.com>
2020-03-05 23:26:39 -0300

    signup e login added

--- sisponto/settings.py
+++ sisponto/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sisponto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'employees.Employee'
+LOGIN_REDIRECT_URL = 'Home Page'
+LOGOUT_REDIRECT_URL = 'Home Page'
